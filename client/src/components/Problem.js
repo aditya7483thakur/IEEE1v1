2d088ba62dbb5081dffd1c03be1998a757759bf4
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-09-09 17:58:05 +0530

    round wise conditional rendering

--- client/src/components/Problem.js
+++ client/src/components/Problem.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import problems from "../static_utils/problems.json";
+import React from "react";
+import rounds from "../static_utils/problems.json";
 
 function Problem({ round_no }) {
   return (
@@ -11,31 +11,40 @@ function Problem({ round_no }) {
         marginLeft: "auto",
         marginRight: "auto",
         padding: "20px",
-        borderRadius: "8px", // Rounded corners for a softer look
+        borderRadius: "8px",
         boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.1)",
       }}
     >
-      {problems.map((problem) => {
-        return (
-          <div key={problem.id}>
-            <h2>{problem.title}</h2>
-            <p>{problem.description}</p>
-            <p>Difficulty: {problem.difficulty}</p>
-            {problem.options.map((option, index) => (
-              <div key={index}>
-                <input
-                  type="radio"
-                  id={`option_${problem.id}_${index}`}
-                  name={problem.id}
-                  value={option.option_id}
-                />
-                <label htmlFor={`option_${problem.id}_${index}`}>
-                  {option.code}{" "}
-                </label>
-              </div>
-            ))}
-          </div>
-        );
+      {rounds.map((round) => {
+        // Check if the current round matches the passed round_no
+        if (round.round_id === round_no) {
+          return (
+            <div key={round.round_id}>
+              {round.problems.map((problem) => (
+                <div key={problem.id}>
+                  <h2>{problem.title}</h2>
+                  <p>{problem.description}</p>
+                  <p>Difficulty: {problem.difficulty}</p>
+                  {problem.options.map((option, index) => (
+                    <div key={index}>
+                      <input
+                        type="radio"
+                        id={`option_${problem.id}_${index}`}
+                        name={problem.id}
+                        value={option.option_id}
+                      />
+                      <label htmlFor={`option_${problem.id}_${index}`}>
+                        {option.code}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          );
+        } else {
+          return null; // Return null if the round does not match
+        }
       })}
     </div>
   );
